e4b4bc097494a3511e4457d1b206713b5f73468b
Vedika Udgir <vedikaudgir@gmail.com>
2025-06-30 23:24:08 +0530

    added DB

---
+++ webApps/EmployeeAdminPortal/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<DbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Employee> MyProperty { get; set; }
+    }
+}
---
+++ webApps/EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public required decimal Salary { get; set; }
+    }
+}
