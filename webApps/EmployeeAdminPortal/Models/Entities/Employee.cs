e46db99dbb8927595b571a96d3645d2a3607e1e8
Vedika Udgir <vedikaudgir@gmail.com>
2025-07-01 00:01:56 +0530

    added DB

--- webApps/EmployeeAdminPortal/Models/Entities/Employee.cs
+++ webApps/EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -6,6 +6,6 @@
         public required string Name { get; set; }
         public required string Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public required decimal Salary { get; set; }
+        public required string Salary { get; set; }
     }
 }
