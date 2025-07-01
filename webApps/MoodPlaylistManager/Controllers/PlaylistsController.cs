35969c11cc9d4339eae6728322d5ad05651d87d0
Vedika Udgir <vedikaudgir@gmail.com>
2025-07-01 17:28:59 +0530

    added migrations

---
+++ webApps/MoodPlaylistManager/Controllers/PlaylistsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoodPlaylistManager.Data;
+using MoodPlaylistManager.Models;
+
+
+namespace MoodPlaylistManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlaylistsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(DbContext.Playlist.ToList());
+        }
+
+    }
+}
---
+++ webApps/MoodPlaylistManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using MoodPlaylistManager.Models.Entities;
+
+namespace MoodPlaylistManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}        
+        public DbSet<Playlists> Playlist { get; set; }
+    }
+}
